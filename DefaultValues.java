ef81f12e4cd87eb6ba7083266b736d27301ecc96
omkar <omkardudhewar121@gmail.com>
2022-11-20 17:49:10 +0530

    added program to check default values of data types

---
+++ DefaultValues.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.Day1Problms;
+
+public class DefaultValues {
+    static int val1;
+    static float val2;
+    static double val3;
+    static long val4;
+    static char val5;
+    static boolean val6;
+    static byte val7;
+    static short val8;
+
+    public static void main(String[] args) {
+        System.out.println("Default value is " + val1);
+        System.out.println("Default value is " + val2);
+        System.out.println("Default value is " + val3);
+        System.out.println("Default value is " + val4);
+        System.out.println("Default value is " + val5);
+        System.out.println("Default value is " + val6);
+        System.out.println("Default value is " + val7);
+        System.out.println("Default value is " + val8);
+
+    }
+}
